59ae204cafa4cededa647145582f697c2c82c9e8
Joyceyte <138740545+Joyceyte@users.noreply.github.com>
2025-04-15 23:13:17 -0700

    fixbug

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import LogsComponent from "./Logs";
 import NavBarComponent from "./NavBar";
 import ProfilePage from "./ProfilePage";
-import LandingPage from "./landingpage/landingpage";
+import LandingPage from "./landingpage/LandingPage";
 
 export default function App() {
   return (
     <div>
+      {/*hi */}
       <NavBarComponent />
       <Routes>
         <Route path="/" element={<LandingPage />} />
