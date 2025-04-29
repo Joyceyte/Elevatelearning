9ea807c70e4853c69472735365c34d3c9f667a81
Joyceyte <138740545+Joyceyte@users.noreply.github.com>
2025-04-29 13:00:43 +1000

    added testimonials

--- src/ibenglish/IbenglishPage.jsx
+++ src/ibenglish/IbenglishPage.jsx
@@ -35,10 +35,7 @@ const IBPage = () => {
   return (
     <div className="bg-slate-50">
       {/* Hero Section */}
-      <section
-        ref={ref}
-        className={`flex items-center py-16 px-12 `}
-      >
+      <section ref={ref} className={`flex items-center py-16 px-12 `}>
         {/* Text Section */}
         <div className="text-center md:text-left md:w-1/2">
           <h1 className="text-6xl font-black font-helvetica text-[#515cb8] pb-12">
@@ -145,44 +142,43 @@ const IBPage = () => {
         </div>
       </section>
 
-   
-
       <section
-      ref={ref}
-      className={`py-12 px-6 transition-opacity duration-1000 ease-in-out ${
-        inView ? "opacity-100" : "opacity-0"
-      }`}
-    >
-      <h2 className="text-3xl font-bold text-[#515cb8] mb-6 text-center">
-        Let’s break down some <span className= "text-[#ff3b6f]">limiting misconceptions</span>
-      </h2>
-      <div className="space-y-4">
-        {misconceptions.map((misconception, index) => (
-          <div
-            key={index}
-            className="border-b border-[#e1e1e1] py-3 px-4 cursor-pointer"
-            onClick={() => toggleAccordion(index)}
-          >
-            <div className="flex items-center justify-between text-lg text-[#515cb8] font-semibold">
-              <span>{misconception.question}</span>
-              {openIndex === index ? (
-                <FaChevronUp className="text-[#ff3b6f]" />
-              ) : (
-                <FaChevronDown className="text-[#ff3b6f]" />
+        ref={ref}
+        className={`py-12 px-6 transition-opacity duration-1000 ease-in-out ${
+          inView ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        <h2 className="text-3xl font-bold text-[#515cb8] mb-6 text-center">
+          Let’s break down some{" "}
+          <span className="text-[#ff3b6f]">limiting misconceptions</span>
+        </h2>
+        <div className="space-y-4">
+          {misconceptions.map((misconception, index) => (
+            <div
+              key={index}
+              className="border-b border-[#e1e1e1] py-3 px-4 cursor-pointer"
+              onClick={() => toggleAccordion(index)}
+            >
+              <div className="flex items-center justify-between text-lg text-[#515cb8] font-semibold">
+                <span>{misconception.question}</span>
+                {openIndex === index ? (
+                  <FaChevronUp className="text-[#ff3b6f]" />
+                ) : (
+                  <FaChevronDown className="text-[#ff3b6f]" />
+                )}
+              </div>
+              {openIndex === index && (
+                <div className="mt-2 text-sm text-[#5c5875]">
+                  {misconception.answer}
+                </div>
               )}
             </div>
-            {openIndex === index && (
-              <div className="mt-2 text-sm text-[#5c5875]">
-                {misconception.answer}
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
-    </section>
-    <LessonTimeline/>
-       {/* Reviews Section */}
-       <section
+          ))}
+        </div>
+      </section>
+      <LessonTimeline />
+      {/* Reviews Section */}
+      <section
         className={`py-16 px-12 transition-opacity duration-1000 ease-in-out ${
           inView ? "opacity-100" : "opacity-0"
         }`}
@@ -202,52 +198,46 @@ const IBPage = () => {
               literally brought my paper 1’s from 10/20 to around 16-18/20."
             </p>
             <div className="flex justify-left text-[#515cb8] font-bold">
-              Joyce L
+              Joyce
             </div>
             <div className="flex justify-left text-[#515cb8] font-bold">
-              2022 - 2023 SL
+              2023 - 2024 SL (High 7)
             </div>
           </div>
 
           {/* Review 2 */}
-          <div className="flex-shrink-0 w-full md:w-1/3 p-4 bg-white rounded-lg shadow-lg">
+          <div className="flex-shrink-0 w-full md:w-1/3 p-4 bg-white rounded-lg shadow-lg flex flex-col">
             <p className="italic text-[#5c5875]">
-              "I loved tutoring with Joyce because she provided everything to
-              help me improve in English. She breaks down examples of top-band
-              essays in an easily understandable and implementable way. She is
-              amazing at giving targeted, personalised feedback, and will hammer
-              it in until top-band writing becomes intuitive. Her tutoring
-              literally brought my paper 1’s from 10/20 to around 16-18/20."
+              "Joyce is so generous with her time and knowledge, and she's done
+              a superb job helping me understand GLA techniques and supporting
+              me through the academic and emotional aspects of IB. (I got 92%
+              for Lit)
             </p>
-            <div className="flex justify-left text-[#515cb8] font-bold">
-              Joyce L
+            <div className="flex justify-left text-[#515cb8] font-bold mt-auto">
+              Rita
             </div>
             <div className="flex justify-left text-[#515cb8] font-bold">
-              2022 - 2023 SL
+              2023 HL (99.95 ATAR)
             </div>
           </div>
 
           {/* Review 3 */}
           <div className="flex-shrink-0 w-full md:w-1/3 p-4 bg-white rounded-lg shadow-lg">
             <p className="italic text-[#5c5875]">
-              "I loved tutoring with Joyce because she provided everything to
-              help me improve in English. She breaks down examples of top-band
-              essays in an easily understandable and implementable way. She is
-              amazing at giving targeted, personalised feedback, and will hammer
-              it in until top-band writing becomes intuitive. Her tutoring
-              literally brought my paper 1’s from 10/20 to around 16-18/20."
+              Joyce's writing formulas and constant feedback really kept me
+              practicing. I’ve learned so much from you!
             </p>
             <div className="flex justify-left text-[#515cb8] font-bold">
-              Joyce L
+              Jenny
             </div>
             <div className="flex justify-left text-[#515cb8] font-bold">
-              2022 - 2023 SL
+              2023 - 2024 SL (99.95 ATAR)
             </div>
           </div>
         </div>
       </section>
 
-    <CTASection/>
+      <CTASection />
     </div>
   );
 };
